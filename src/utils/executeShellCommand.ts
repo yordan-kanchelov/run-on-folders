2d816fc0fbf2854bd6453b89f509323ae4cea259
Yordan Kanchelov <yordan.kanchelov@gmail.com>
2022-12-12 13:27:33 +0200

    feat: display the process cwd in front of the response

--- src/utils/executeShellCommand.ts
+++ src/utils/executeShellCommand.ts
@@ -5,17 +5,14 @@ import * as log from "fancy-log";
 
 const execPromise = util.promisify(exec);
 
-export default async function executeShellCommand(
-    command: string,
-    options: ExecOptions
-): Promise<void> {
+export default async function executeShellCommand(command: string, options: ExecOptions): Promise<void> {
     log(chalk.red(`EXECUTING -  ${command} on ${options.cwd}`));
 
     try {
         const { stdout, stderr } = await execPromise(command, options);
-        log("stdout:", stdout);
-        log("stderr:", stderr);
+        log(`${chalk.red(`[${options.cwd}]`)} stdout:`, stdout);
+        log(`${chalk.red(`[${options.cwd}]`)} stderr:`, stderr);
     } catch (e) {
-        log.error("exception:", e);
+        log.error(`${chalk.red(`[${options.cwd}]`)} exception:`, e);
     }
 }
