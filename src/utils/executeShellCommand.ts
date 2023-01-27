711fa7864e01bca07df9349bcce4fc9d5335bdcd
Yordan Kanchelov <yordan.kanchelov@gmail.com>
2023-01-27 17:33:15 +0000

    feat: handle case when stderr or stdout are empty strings

--- src/utils/executeShellCommand.ts
+++ src/utils/executeShellCommand.ts
@@ -5,13 +5,30 @@ import * as log from "fancy-log";
 
 const execPromise = util.promisify(exec);
 
-export default async function executeShellCommand(command: string, options: ExecOptions): Promise<void> {
+export default async function executeShellCommand(
+    command: string,
+    options: ExecOptions
+): Promise<void> {
     log(chalk.red(`EXECUTING -  ${command} on ${options.cwd}`));
 
     try {
         const { stdout, stderr } = await execPromise(command, options);
-        log(`${chalk.red(`[${options.cwd}]`)} stdout:`, stdout);
-        log(`${chalk.red(`[${options.cwd}]`)} stderr:`, stderr);
+
+        if (stdout) {
+            log(`${chalk.red(`[${options.cwd}]`)} stdout:`, stdout);
+        }
+
+        if (stderr) {
+            log(`${chalk.red(`[${options.cwd}]`)} stderr:`, stderr);
+        }
+
+        if (!stderr && !stdout) {
+            log(
+                `${chalk.red(
+                    `[${options.cwd}]`
+                )} command finished without any output`
+            );
+        }
     } catch (e) {
         log.error(`${chalk.red(`[${options.cwd}]`)} exception:`, e);
     }
